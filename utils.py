d041e1b12bc2cf2af41e94f590d47476ce4cdafa
ryryrymln88808 <ryryrymln88808@gmail.com>
2024-06-24 09:25:15 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -734,7 +734,7 @@ async def send_all(bot, userid, files, ident):
                             InlineKeyboardButton("Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ", url=GRP_LNK),
                             InlineKeyboardButton("Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ", url=CHNL_LNK),
                         ],
-                        [InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/creatorbeatz")],
+                        [InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/RS_Movie")],
                     ]
                 ),
             )
@@ -848,7 +848,7 @@ async def send_all(bot, userid, files, ident, chat_id, user_name, query):
                                 InlineKeyboardButton("Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ", url=GRP_LNK),
                                 InlineKeyboardButton("Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ", url=CHNL_LNK),
                             ],
-                            [InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Kgashok04")],
+                            [InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/RS_Movie")],
                         ]
                     ),
                 )
@@ -890,7 +890,7 @@ async def send_all(bot, userid, files, ident, chat_id, user_name, query):
                         InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                         InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                     ],[
-                        InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Kgashok04")
+                        InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/RS_Movie")
                         ]
                     ]
                 )
